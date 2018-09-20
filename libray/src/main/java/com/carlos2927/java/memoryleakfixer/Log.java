6e23820911533e208f890ebe6ae25cb0bd74e739
Carlos <yonggong615@gmail.com>
2018-09-20 17:27:17 +0800

    Update Log.java

--- libray/src/main/java/com/carlos2927/java/memoryleakfixer/Log.java
+++ libray/src/main/java/com/carlos2927/java/memoryleakfixer/Log.java
@@ -3,45 +3,47 @@ package com.carlos2927.java.memoryleakfixer;
 import java.lang.reflect.Method;
 
 public class Log {
-    private static Method[] androidLogMethod = null;
+    //private static Method[] androidLogMethod = null;
     public static void d(String tag,String msg){
         if(AppEnv.IsInAndroidPlatform){
-            if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
-                if(androidLogMethod == null){
-                    androidLogMethod = new Method[5];
-                }
-                try {
-                    if(androidLogMethod[1] == null){
-                        androidLogMethod[1] = Class.forName("android.util.Log").getDeclaredMethod("d",String.class,String.class);
-                    }
-                    androidLogMethod[1].invoke(null,tag,msg);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }else {
-                android.util.Log.d(tag,msg);
-            }
+//             if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
+//                 if(androidLogMethod == null){
+//                     androidLogMethod = new Method[5];
+//                 }
+//                 try {
+//                     if(androidLogMethod[1] == null){
+//                         androidLogMethod[1] = Class.forName("android.util.Log").getDeclaredMethod("d",String.class,String.class);
+//                     }
+//                     androidLogMethod[1].invoke(null,tag,msg);
+//                 }catch (Exception e){
+//                     e.printStackTrace();
+//                 }
+//             }else {
+//                 android.util.Log.d(tag,msg);
+//             }
+            android.util.Log.d(tag,msg);
         }else {
             JavaLog.print(tag,"Debug",msg);
         }
     }
     public static void i(String tag,String msg){
         if(AppEnv.IsInAndroidPlatform){
-            if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
-                if(androidLogMethod == null){
-                    androidLogMethod = new Method[5];
-                }
-                try {
-                    if(androidLogMethod[2] == null){
-                        androidLogMethod[2] = Class.forName("android.util.Log").getDeclaredMethod("i",String.class,String.class);
-                    }
-                    androidLogMethod[2].invoke(null,tag,msg);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }else {
-                android.util.Log.i(tag,msg);
-            }
+//             if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
+//                 if(androidLogMethod == null){
+//                     androidLogMethod = new Method[5];
+//                 }
+//                 try {
+//                     if(androidLogMethod[2] == null){
+//                         androidLogMethod[2] = Class.forName("android.util.Log").getDeclaredMethod("i",String.class,String.class);
+//                     }
+//                     androidLogMethod[2].invoke(null,tag,msg);
+//                 }catch (Exception e){
+//                     e.printStackTrace();
+//                 }
+//             }else {
+//                 android.util.Log.i(tag,msg);
+//             }
+            android.util.Log.i(tag,msg);
         }else {
             JavaLog.print(tag,"Info",msg);
         }
@@ -51,21 +53,22 @@ public class Log {
         if(AppEnv.IsInAndroidPlatform){
             //在JavaMemoryLeakFixer的androidDemo中这里直接调用android.util.Log.(tag,msg)报错，为什么？
             // java.lang.NoSuchMethodError: No static method w(Ljava/lang/String;Ljava/lang/String;)V in class Landroid/util/Log; or its super classes (declaration of 'android.util.Log' appears in /system/framework/framework.jar:classes2.dex)
-            if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
-                if(androidLogMethod == null){
-                    androidLogMethod = new Method[5];
-                }
-                try {
-                    if(androidLogMethod[3] == null){
-                        androidLogMethod[3] = Class.forName("android.util.Log").getDeclaredMethod("w",String.class,String.class);
-                    }
-                    androidLogMethod[3].invoke(null,tag,msg);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }else {
-                android.util.Log.w(tag,msg);
-            }
+//             if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
+//                 if(androidLogMethod == null){
+//                     androidLogMethod = new Method[5];
+//                 }
+//                 try {
+//                     if(androidLogMethod[3] == null){
+//                         androidLogMethod[3] = Class.forName("android.util.Log").getDeclaredMethod("w",String.class,String.class);
+//                     }
+//                     androidLogMethod[3].invoke(null,tag,msg);
+//                 }catch (Exception e){
+//                     e.printStackTrace();
+//                 }
+//             }else {
+//                 android.util.Log.w(tag,msg);
+//             }
+            android.util.Log.w(tag,msg);
         }else {
             JavaLog.print(tag,"Warn",msg);
         }
@@ -73,21 +76,22 @@ public class Log {
 
     public static void e(String tag,String msg){
         if(AppEnv.IsInAndroidPlatform){
-            if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
-                if(androidLogMethod == null){
-                    androidLogMethod = new Method[5];
-                }
-                try {
-                    if(androidLogMethod[4] == null){
-                        androidLogMethod[4] = Class.forName("android.util.Log").getDeclaredMethod("e",String.class,String.class);
-                    }
-                    androidLogMethod[4].invoke(null,tag,msg);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }else {
-                android.util.Log.e(tag,msg);
-            }
+//             if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
+//                 if(androidLogMethod == null){
+//                     androidLogMethod = new Method[5];
+//                 }
+//                 try {
+//                     if(androidLogMethod[4] == null){
+//                         androidLogMethod[4] = Class.forName("android.util.Log").getDeclaredMethod("e",String.class,String.class);
+//                     }
+//                     androidLogMethod[4].invoke(null,tag,msg);
+//                 }catch (Exception e){
+//                     e.printStackTrace();
+//                 }
+//             }else {
+//                 android.util.Log.e(tag,msg);
+//             }
+            android.util.Log.e(tag,msg);
         }else {
             JavaLog.print(tag,"Error",msg);
         }
@@ -95,21 +99,22 @@ public class Log {
 
     public static void v(String tag,String msg){
         if(AppEnv.IsInAndroidPlatform){
-            if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
-                if(androidLogMethod == null){
-                    androidLogMethod = new Method[5];
-                }
-                try {
-                    if(androidLogMethod[0] == null){
-                        androidLogMethod[0] = Class.forName("android.util.Log").getDeclaredMethod("v",String.class,String.class);
-                    }
-                    androidLogMethod[0].invoke(null,tag,msg);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }else {
-                android.util.Log.v(tag,msg);
-            }
+//             if(AppEnv.IsUseInJavaMemoryLeakFixSourceTest){
+//                 if(androidLogMethod == null){
+//                     androidLogMethod = new Method[5];
+//                 }
+//                 try {
+//                     if(androidLogMethod[0] == null){
+//                         androidLogMethod[0] = Class.forName("android.util.Log").getDeclaredMethod("v",String.class,String.class);
+//                     }
+//                     androidLogMethod[0].invoke(null,tag,msg);
+//                 }catch (Exception e){
+//                     e.printStackTrace();
+//                 }
+//             }else {
+//                 android.util.Log.v(tag,msg);
+//             }
+            android.util.Log.v(tag,msg);
         }else {
             JavaLog.print(tag,"Verbose",msg);
         }
