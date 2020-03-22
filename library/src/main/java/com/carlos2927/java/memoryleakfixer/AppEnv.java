d1ae73323335701184add3c01815994d34d7f54b
Carlos <15574979960@163.com>
2020-03-22 23:07:25 +0800

    Update AppEnv.java

--- library/src/main/java/com/carlos2927/java/memoryleakfixer/AppEnv.java
+++ library/src/main/java/com/carlos2927/java/memoryleakfixer/AppEnv.java
@@ -71,7 +71,7 @@ public class AppEnv {
     }.call();
 
     public static final int LibVersionCode = 21;
-    public static final String LibVersion = "v1.2.4-beta5";
+    public static final String LibVersion = "v1.2.4";
 
     /**
      * InnerClassHelper.InnerClassTargetList列表无数据时循环检测线程休眠时间
